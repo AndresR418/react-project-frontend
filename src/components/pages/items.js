e44e79a7f28db1c9817ded83641217e98667152e
Andres <andresrodriguez418@gmail.com>
2021-12-21 00:06:14 -0500

    Heroku demo push

---
+++ src/components/pages/items.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export default class Items extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            loading: true,
+            error: false
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://backend-demo-ar.herokuapp.com/item/get")
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                items: data,
+                loading: false
+            });
+        })
+        .catch(error => {
+            this.setState({
+                error: true,
+                loading: false
+            })
+        })
+    }
+
+    renderItems() {
+        const itemsHTML = this.state.items.map(items => {
+            return (
+            <div className='item-wrapper'>
+                <h3 className="title">{items.name}</h3>
+                <p className="description">
+                    ${items.price.toFixed(2)}
+                </p>
+            </div>)
+        })
+
+        return itemsHTML;
+    }
+
+    render() {
+        console.log(this.state.items)
+        return (
+            <div>{this.renderItems()}</div>
+        )
+    }
+}
